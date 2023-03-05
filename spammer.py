b670e54c7c4158aa9353a8d9f7a7bd29803d07a3
Turner1a <126750784+Turner1a@users.noreply.github.com>
2023-03-04 18:32:16 -0800

    Create spammer.py

---
+++ spammer.py
@@ -0,0 +1,12 @@
+#!/bin/pyhton
+
+# - spammer v2
+#   Description: spams a phone number by sendind it a lot of sms by using GRAB API
+#   Author: p4kL0c4t
+#   Date: 5/12/2017
+#   Disclaimer: Editing author will not make you the real coder
+
+imort spammer_class
+spammer = spammer_class.spammer()
+spammer.author = "p4kL0nc4t
+spammer.main()
